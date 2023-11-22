a88acdcff9e6f054c64f842739a436578f7749a4
GZTime <Time.GZ@outlook.com>
2023-11-23 01:01:53 +0800

    docs: update

--- pkg/boot/src/main.rs
+++ pkg/boot/src/main.rs
@@ -1,12 +1,3 @@
-//! Simple ELF OS Loader on UEFI @ 2022.03.23
-//!
-//! 1. Load config from "\EFI\Boot\rboot.conf"
-//! 2. Load kernel ELF file
-//! 3. Map ELF segments to virtual memory
-//! 4. Map kernel stack and all physical memory
-//! 5. Startup all processors
-//! 6. Exit boot and jump to ELF entry
-
 #![no_std]
 #![no_main]
 #![feature(alloc_error_handler)]
@@ -40,6 +31,7 @@ fn efi_main(image: uefi::Handle, mut system_table: SystemTable<Boot>) -> Status 
     log::set_max_level(log::LevelFilter::Info);
     info!("Running UEFI bootloader...");
 
+    // 1. Load config
     let bs = system_table.boot_services();
     let config = {
         let mut file = open_file(bs, CONFIG_PATH);
@@ -49,6 +41,7 @@ fn efi_main(image: uefi::Handle, mut system_table: SystemTable<Boot>) -> Status 
 
     info!("config: {:#x?}", config);
 
+    // 2. Load ELF files
     let elf = {
         let mut file = open_file(bs, config.kernel_path);
         let buf = load_file(bs, &mut file);
@@ -58,6 +51,7 @@ fn efi_main(image: uefi::Handle, mut system_table: SystemTable<Boot>) -> Status 
         ENTRY = elf.header.pt2.entry_point() as usize;
     }
 
+    // 3. Load MemoryMap
     let max_mmap_size = system_table.boot_services().memory_map_size();
     let mmap_storage = Box::leak(
         vec![0; max_mmap_size.map_size + 10 * max_mmap_size.entry_size].into_boxed_slice(),
@@ -74,9 +68,9 @@ fn efi_main(image: uefi::Handle, mut system_table: SystemTable<Boot>) -> Status 
         .unwrap()
         .max(0x1_0000_0000); // include IOAPIC MMIO area
 
+    // 4. Map ELF segments, kernel stack and physical memory to virtual memory
     let mut page_table = current_page_table();
-    // root page table is readonly
-    // disable write protect
+    // root page table is readonly, disable write protect
     unsafe {
         Cr0::update(|f| f.remove(Cr0Flags::WRITE_PROTECT));
         Efer::update(|f| f.insert(EferFlags::NO_EXECUTE_ENABLE));
@@ -119,6 +113,7 @@ fn efi_main(image: uefi::Handle, mut system_table: SystemTable<Boot>) -> Status 
         Cr0::update(|f| f.insert(Cr0Flags::WRITE_PROTECT));
     }
 
+    // 5. Exit boot and jump to ELF entry
     info!("Exiting boot services...");
 
     let (runtime, mmap) = system_table.exit_boot_services(MemoryType::LOADER_DATA);
