636899751d254180f719220a2d1b7a753185036a
GZTime <Time.GZ@outlook.com>
2023-11-23 14:44:50 +0800

    wip: update makefile

--- pkg/kernel/src/memory/allocator.rs
+++ pkg/kernel/src/memory/allocator.rs
@@ -3,7 +3,7 @@
 use linked_list_allocator::LockedHeap;
 use x86_64::VirtAddr;
 
-pub const HEAP_SIZE: usize = 1024 * 1024; // 1 MiB
+pub const HEAP_SIZE: usize = 8 * 1024 * 1024; // 8 MiB
 
 #[global_allocator]
 pub static ALLOCATOR: LockedHeap = LockedHeap::empty();
@@ -14,15 +14,18 @@ pub fn init() {
     let heap_start = VirtAddr::from_ptr(unsafe { HEAP.as_ptr() });
     let heap_end = heap_start + HEAP_SIZE;
 
+    unsafe {
+        ALLOCATOR.lock().init(HEAP.as_mut_ptr(), HEAP_SIZE);
+    }
+
     debug!(
         "Kernel Heap      : 0x{:016x}-0x{:016x}",
         heap_start.as_u64(),
         heap_end.as_u64()
     );
 
-    unsafe {
-        ALLOCATOR.lock().init(HEAP.as_mut_ptr(), HEAP_SIZE);
-    }
+    let (size, unit) = super::humanized_size(HEAP_SIZE as u64);
+    info!("Kernel Heap Size : {:>7.*} {}", 3, size, unit);
 
     info!("Kernel Heap Initialized.");
 }
