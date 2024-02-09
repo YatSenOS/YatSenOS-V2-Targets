e0847a6214a63d05fc4a9ea269f54a571f62074d
GZTime <Time.GZ@outlook.com>
2024-02-09 14:48:31 +0800

    wip: mv `config` to `config.toml`

--- pkg/kernel/src/interrupt/apic/mod.rs
+++ pkg/kernel/src/interrupt/apic/mod.rs
@@ -14,6 +14,7 @@ pub use xapic::{XApic, LAPIC_ADDR};
 mod ioapic;
 mod xapic;
 
+#[allow(dead_code)]
 pub trait LocalApic {
     /// If this type APIC is supported
     fn support() -> bool;
@@ -29,6 +30,7 @@ pub trait LocalApic {
     /// Interrupt Command Register
     fn icr(&self) -> u64;
 
+    /// Set Interrupt Command Register
     fn set_icr(&mut self, value: u64);
 
     /// Acknowledge interrupt on the current CPU
