f00151b0d432b6b73f59ce69e845d1939d969bf8
GZTime <Time.GZ@outlook.com>
2024-02-24 16:03:47 +0800

    chore: make ysos happy

--- pkg/kernel/src/lib.rs
+++ pkg/kernel/src/lib.rs
@@ -40,7 +40,6 @@ pub fn init(boot_info: &'static BootInfo) {
     memory::init(boot_info); // init memory manager
     memory::user::init(); // init user heap allocator
     proc::init(boot_info); // init task manager
-    input::init(); // init input
     ata::init(); // init ata
     filesystem::init(); // init filesystem
 
