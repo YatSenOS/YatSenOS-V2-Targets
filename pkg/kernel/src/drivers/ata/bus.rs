84392467b202a118ccc9e2f38f65b2fd7085cfcb
GZTime <Time.GZ@outlook.com>
2024-03-18 20:06:51 +0800

    rename: `Bus` -> `AtaBus`

--- pkg/kernel/src/drivers/ata/bus.rs
+++ pkg/kernel/src/drivers/ata/bus.rs
@@ -11,7 +11,7 @@ use x86_64::instructions::port::*;
 
 #[derive(Debug, Clone)]
 #[allow(dead_code)]
-pub struct Bus {
+pub struct AtaBus {
     id: u8,
     irq: u8,
     io_base: u16,
@@ -34,7 +34,7 @@ pub struct Bus {
     drive_blockess: PortReadOnly<u8>,
 }
 
-impl Bus {
+impl AtaBus {
     pub fn new(id: u8, irq: u8, io_base: u16, ctrl_base: u16) -> Self {
         Self {
             id,
